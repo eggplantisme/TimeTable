ecf6870989d98890a61929b0ecce4d51b498b7f7
eggplant <2439166979@qq.com>
2017-05-16 15:19:59 +0800

    first update

---
+++ app/src/main/java/com/example/admin/timetable/Course.java
@@ -0,0 +1,101 @@
+package com.example.admin.timetable;
+
+import java.io.Serializable;
+
+/**
+ * Created by admin on 2016/12/8.
+ */
+public class Course implements Serializable {
+    private String name;
+    private String teacher;
+    private String email_ftp;
+    private int week;
+    private int start;
+    private int end;
+    private String address;
+    private String ddl;
+    private String ddl_time;
+    Course() {
+        name = "";
+        teacher = "";
+        email_ftp = "";
+        week = 0;
+        start = 0;
+        end = 0;
+        address = "";
+        ddl = "";
+        ddl_time = "";
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDdl() {
+        return ddl;
+    }
+
+    public String getEmail_ftp() {
+        return email_ftp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDdl(String ddl) {
+        this.ddl = ddl;
+    }
+
+    public void setEmail_ftp(String email_ftp) {
+        this.email_ftp = email_ftp;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public String getDdl_time() {
+        return ddl_time;
+    }
+
+    public void setDdl_time(String ddl_time) {
+        this.ddl_time = ddl_time;
+    }
+}
